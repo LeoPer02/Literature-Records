2c7ba6b8069d97d2a3dee641b20dbc0d0b856fc1
LeoPer02 <leocoding.p@hotmail.com>
2024-12-13 21:57:22 +0000

    Initial Commit

---
+++ literatureRecords/src/main/java/com/leoper02/literatureRecords/LiteratureRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.leoper02.literatureRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratureRecordsApplication.class, args);
+	}
+
+}
---
+++ literatureRecords/src/test/java/com/leoper02/literatureRecords/LiteratureRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoper02.literatureRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteratureRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
